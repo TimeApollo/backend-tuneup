3acfee198edc5c1a8f5802660b33c557871b4e74
TimeApollo <arnjcksn0845@yahoo.com>
2018-11-28 14:50:12 -0500

    finished all three parts. left the timeit help uncommented so it runs too

--- tuneup.py
+++ tuneup.py
@@ -6,12 +6,27 @@ __author__ = "???"
 
 import cProfile
 import pstats
+import timeit
 
 
 def profile(func):
     """A function that can be used as a decorator to meausre performance"""
 
-    raise NotImplementedError("Complete this decorator function")
+    def profile_function(*args, **kwargs):
+
+        pr = cProfile.Profile()
+        pr.enable()
+
+        result = func(*args, **kwargs)
+
+        pr.disable()
+
+        sortby = 'cumulative'
+        ps = pstats.Stats(pr).sort_stats(sortby)
+        ps.print_stats()
+
+        return result
+    return profile_function
 
 
 def read_movies(src):
@@ -29,20 +44,41 @@ def is_duplicate(title, movies):
     return False
 
 
+# def find_duplicate_movies(src):
+#     """Returns a list of duplicate movies from a src list"""
+#     movies = read_movies(src)
+#     duplicates = []
+#     while movies:
+#         movie = movies.pop()
+#         if is_duplicate(movie, movies):
+#             duplicates.append(movie)
+#     return duplicates
+
+
+@profile
 def find_duplicate_movies(src):
     """Returns a list of duplicate movies from a src list"""
     movies = read_movies(src)
     duplicates = []
-    while movies:
-        movie = movies.pop()
-        if is_duplicate(movie, movies):
+    movie_dict = {}
+    for movie in movies:
+        if movie in movie_dict:
             duplicates.append(movie)
+        else:
+            movie_dict[movie] = None
     return duplicates
 
 
 def timeit_helper():
     """Part A:  Obtain some profiling measurements using timeit"""
     # YOUR CODE GOES HERE
+    t = timeit.Timer("find_duplicate_movies('movies.txt')",
+                     setup='from __main__ import find_duplicate_movies')
+    repeat = 2
+    number = 2
+    result = min(t.repeat(repeat=repeat, number=number))/number
+    print('Best time across {} repeats of {} runs per repeat: {} sec'.format(
+        repeat, number, result))
 
 
 def main():
@@ -50,6 +86,7 @@ def main():
     result = find_duplicate_movies('movies.txt')
     print('Found {} duplicate movies:'.format(len(result)))
     print('\n'.join(result))
+    timeit_helper()
 
 
 if __name__ == '__main__':
